f95c6130237f87d67c4b1cf4ae00ee49511a0263
Kimbro Staken <kstaken@kstaken.com>
2013-06-25 10:26:56 -0700

    Adding setup script for distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='salttest',
+    version='0.1',
+    description='Testing Framework for Salt stack modules and states',
+    author='Kimbro Staken',
+    author_email='kstaken@kstaken.com',
+    url='http://github.com/kstaken/salt-test-runner',
+    packages=['salttest'],
+)
