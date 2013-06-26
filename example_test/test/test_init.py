2511adc91a2ba006606f31dbbc33080964846494
Kimbro Staken <kstaken@kstaken.com>
2013-06-26 16:17:02 -0700

    Just adding a test to check the second container

--- example_test/test/test_init.py
+++ example_test/test/test_init.py
@@ -24,5 +24,8 @@ class TestInit(unittest.TestCase):
     context = self._containers.get('example_test')
     self.assertEquals(context.salt_client.cmd(context.build_tag, 'cmd.run', ['cat /tmp/test-file'])[context.build_tag], 'Just a test')
 
+    context = self._containers.get('example_test2')
+    self.assertEquals(context.salt_client.cmd(context.build_tag, 'cmd.run', ['cat /tmp/test-file'])[context.build_tag], 'Just a test')
+
 if __name__ == '__main__':
     unittest.main()
